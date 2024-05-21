a983c9ed58c74bf5d9b2027ea89b65f99fca9501
Zhukov, Alexander P <zhukovap@ornl.gov>
2024-05-21 12:11:23 -0400

    GH CI for conda

--- .github/workflows/build-conda.sh
+++ .github/workflows/build-conda.sh
@@ -1,4 +1,5 @@
 . /opt/conda/etc/profile.d/conda.sh
 conda env create -n pyorbit --file environment.yml
 conda activate pyorbit
-pip install .
+pip install -r requirements.txt
+pip install --no-build-isolation --editable .
