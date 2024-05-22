8205690c06605774ed6731a8acf430007bb6d223
Zhukov, Alexander P <zhukovap@ornl.gov>
2024-05-22 13:15:22 -0400

    Dynamic version to work

--- .github/workflows/build-conda.sh
+++ .github/workflows/build-conda.sh
@@ -1,5 +1,5 @@
 . /opt/conda/etc/profile.d/conda.sh
 conda env create -n pyorbit --file environment.yml
 conda activate pyorbit
-pip install meson-python
+pip install -U meson-python setuptools setuptools-scm
 pip install --no-build-isolation --editable .
