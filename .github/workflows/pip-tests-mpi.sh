399ef1a32ece355e0c8b679c95f01d3a477e36d3
Zhukov, Alexander P <zhukovap@ornl.gov>
2025-04-21 15:32:24 -0400

    returning status

--- .github/workflows/pip-tests-mpi.sh
+++ .github/workflows/pip-tests-mpi.sh
@@ -1,4 +1,9 @@
 . .po3/bin/activate
 cd examples/MPI_Tests
-mpirun -np 2 python mpi_initialization_test.py
-exit $?
+mpirun -np 2 python mpi_initialization_test.py > test.txt
+
+calculated_sha1=$(sha1sum test.txt | awk '{ print $1 }')
+echo $calculated_sha1
+
+[ "$calculated_sha1" == "7737497b7e3e71d18034a9bdaad79b72f9ab80aa" ] && exit 0 || exit 1
+
