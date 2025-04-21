7295e9dc97fc200847591ed2e9734d14cdb4583d
Zhukov, Alexander P <zhukovap@ornl.gov>
2025-04-21 16:49:20 -0400

    different MPI tests

--- .github/workflows/pip-tests-mpi.sh
+++ .github/workflows/pip-tests-mpi.sh
@@ -1,10 +1,31 @@
 . .po3/bin/activate
-cd examples/MPI_Tests
-mpirun -np 2 python mpi_initialization_test.py | tee test.txt
 
+# Simple test to check if MPI was linked at all
+mpirun -np 2 python examples/MPI_Tests/mpi_initialization_test.py | tee test.txt
 calculated_sha1=$(sha1sum test.txt | awk '{ print $1 }')
-echo $calculated_sha1
-cat test.txt
+[ "$calculated_sha1" != "d94e03044bf06c7a42d07505b50fa58b4b30e49a" ] && exit 1
 
-[ "$calculated_sha1" == "d94e03044bf06c7a42d07505b50fa58b4b30e49a" ] && exit 0 || exit 1
+# Uniformly charged sphere
+mpirun -np 2 python examples/SpaceCharge/sc3D/sc_3d_drift_latt_uniform_sphere_bunch.py --SC ellipsoid| tee results.txt
+python examples/SpaceCharge/sc3D/read_sc_numbers.py
+[ $? != 0 ] && exit 1
+
+mpirun -np 2 python examples/SpaceCharge/sc3D/sc_3d_drift_latt_uniform_sphere_bunch.py --SC fft3d| tee results.txt
+python examples/SpaceCharge/sc3D/read_sc_numbers.py
+[ $? != 0 ] && exit 1
+
+# this should fail as no space charge will give incorrect sphere size
+mpirun -np 2 python examples/SpaceCharge/sc3D/sc_3d_drift_latt_uniform_sphere_bunch.py --SC none| tee results.txt
+python examples/SpaceCharge/sc3D/read_sc_numbers.py
+[ $? == 0 ] && exit 1
+
+
+# This tests that  five nodes give the same result as one node
+mpirun -np 1 python examples/SpaceCharge/sc3D/sc_3d_drift_latt_uniform_sphere_bunch.py | tee results.txt
+calculated_sha1=$(sha1sum results.txt | awk '{ print $1 }')
+
+mpirun -np 5 python examples/SpaceCharge/sc3D/sc_3d_drift_latt_uniform_sphere_bunch.py | tee results.txt
+calculated_sha5=$(sha1sum results.txt | awk '{ print $1 }')
+
+[ "$calculated_sha1" != "$calculated_sha5" ] && exit 1
 
