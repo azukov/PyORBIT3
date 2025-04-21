7d40acc313ba82ff8440c2edcc174019e000ea5a
Zhukov, Alexander P <zhukovap@ornl.gov>
2025-04-21 14:55:58 -0400

    Troubleshoot mpi

--- .github/workflows/pip-build-mpich.sh
+++ .github/workflows/pip-build-mpich.sh
@@ -1,5 +1,7 @@
 python3 -m venv .po3
+sed  -i 's|PATH="\$VIRTUAL_ENV\/"bin":\$PATH"|PATH="\$VIRTUAL_ENV\/"bin":\$PATH"\nsource /etc/profile.d/modules.sh\nmodule load mpi/mpich-x86_64|' .po3/bin/activate
 . .po3/bin/activate
+mpirun -V
 pip install -U pip
 pip install -r requirements.txt
 pip install -U setuptools
